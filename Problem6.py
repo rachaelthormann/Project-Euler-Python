452f73faa8116e45de2ece62ddce8ae98a021338
rachaelthormann <rachaelthormann@gmail.com>
2017-06-06 09:02:44 -0400

    Layout for problem 6.

---
+++ Problem6.py
@@ -0,0 +1,21 @@
+"""
+File name: Problem6.py
+Description: Find the difference between the sum of the squares of the first one hundred
+             natural numbers and the square of the sum.
+Author: Rachael Thormann
+Date Created: 6/6/2017
+Date Last Modified: 6/6/2017
+Python Version: 3.4
+"""
+
+
+def sum_of_squares():
+    pass
+
+
+def square_of_sum():
+    pass
+
+
+def difference():
+    pass
